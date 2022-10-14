e6c28329e0a176f816b29b54bfdedf1366f23891
Christian <christianstewart5111@gmail.com>
2022-10-14 20:44:10 +0800

    Added JS Scroll Show Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,6 +60,12 @@ const scrollActive = () =>{
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
