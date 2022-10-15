d2756874d1d4a8a18c91789a9e88e2259dcf0178
Christian <christianstewart5111@gmail.com>
2022-10-15 14:02:31 +0800

    Added scroll Reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,3 +69,18 @@ const scrollUp = () =>{
 window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true //Animations repeat
+})
+
+sr.reveal(`.home__data, .footer__content, .footer__logo, .footer__description`)
+sr.reveal(`.home__tree-1`, {origin: 'left', delay:800})
+sr.reveal(`.home__tree-2`, {origin: 'right', delay:800})
+sr.reveal(`.home__img`, {delay: 800})
+sr.reveal(`.category__card, .items__card`, {interval: 100})
+sr.reveal(`.about__img, .about__data, .footer__tree-2`, {origin: 'left'})
+sr.reveal(`.party__images, .party__data, .footer__tree-1`, {origin: 'right'})
